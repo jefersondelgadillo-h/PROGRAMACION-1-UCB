bf9ce45c03ab182a251167d09579d4667ffb0bdc
Jeferson A. Delgadillo Helmer <jeferson.delgadillo@ucb.edu.bo>
2025-10-10 10:42:04 -0400

    Add files via upload

---
+++ progra_1/MATRIX/matrix.js
@@ -0,0 +1,50 @@
+class Matrix {
+    rows;
+    cols;
+    data;
+
+    constructor (rowsParam, colsParam, defaultValue = 0) {
+        this. rows = rowsParam;
+        this.cols = colsParam;
+        this.data = [];
+
+        for(let i = 0; i< rowsParam;i++){
+            const rowTemp = [];
+            for (let j = 0; j< this.cols; j++) {
+                rowTemp.push(defaultValue);
+            }
+            this.data.push(row);
+        }
+    }
+
+    setValue (row,col,value){
+        if (isValidPosition(row, col)){
+    this.data[row][col] = value;
+    }
+    }
+    
+    //funcion de validacion de rango valido en la matriz
+    isValidPosition(row, col) {
+        return cow >= 0 && row < this.rows && col>= 0 && col < this.cols;
+    }
+    getValue(row,col){
+         if (this.isValidPosition(row, col)){
+        return this.data[row][col];
+         }else{
+            return null 
+         }
+    }
+    fillRandom (min,max) {
+        for (let i=0; i< this.rows; i++){
+            for (let j=0; j< this.cols; j++){
+                const random = Math.floor (Math.random()*(max-min+1))+min;
+                this.data [i][j]=random;
+            }
+        }
+    }
+    
+    toString () {
+        return this.data.map(row => row.join("\t")).join ("\n");
+    }
+    
+    }
